461552bcacf100c3888a73e34db16e542af4cc0f
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-08-19 02:04:47 +0800

    add bootstrap methods attribute

--- vjvm/classfile/attribute_info.v
+++ vjvm/classfile/attribute_info.v
@@ -109,6 +109,38 @@ fn (attr &SourceFileAttribute) file_name() !string {
 }
 
 /*
+BootstrapMethods_attribute {
+    u2 attribute_name_index;
+    u4 attribute_length;
+    u2 num_bootstrap_methods;
+    {   u2 bootstrap_method_ref;
+        u2 num_bootstrap_arguments;
+        u2 bootstrap_arguments[num_bootstrap_arguments];
+    } bootstrap_methods[num_bootstrap_methods];
+}
+*/
+struct BootstrapMethodsAttribute {
+mut:
+	bootstrap_methods []BootstrapMethod = []BootstrapMethod{}
+}
+
+fn (mut attr BootstrapMethodsAttribute) read_info(mut reader ClassReader) ! {
+	len := reader.read_u16()
+	attr.bootstrap_methods = []BootstrapMethod{cap:len}
+
+	for _ in 0 .. len {
+		method_ref := reader.read_u16()
+		arguments := reader.read_u16_array()
+		attr.bootstrap_methods << BootstrapMethod{method_ref, arguments}
+	}
+}
+
+struct BootstrapMethod {
+	bootstrap_method_ref u16
+	bootstrap_arguments []u16
+}
+
+/*
 Exceptions_attribute {
     u2 attribute_name_index;
     u4 attribute_length;
