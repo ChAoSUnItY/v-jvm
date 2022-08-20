c043b6cdb227e3bdb02b0a86dc68b285e358f874
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-08-20 16:25:27 +0800

    cast u32 to int

--- vjvm/instruction/load/load.v
+++ vjvm/instruction/load/load.v
@@ -3,7 +3,7 @@ module load
 import vjvm.rtda { Frame }
 
 fn load<T>(mut frame Frame, index u32) ! {
-	val := frame.local_vars().get<T>(index)!
+	val := frame.local_vars().get<T>(int(index))!
 	mut stack := frame.opreand_stack()
 	stack.push<T>(val)
 }
