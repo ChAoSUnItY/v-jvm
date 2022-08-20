5ddcf736f2cdad7798f91b5a8162eba6d600b94f
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-08-20 15:38:59 +0800

    complete ipush instructions

--- vjvm/instruction/constant/ipush.v
+++ vjvm/instruction/constant/ipush.v
@@ -1,3 +1,32 @@
 module constant
 
-m
+import vjvm.rtda { Frame }
+import vjvm.instruction.base { BytecodeReader }
+
+pub struct BIPUSH {
+mut:
+	val i8
+}
+
+pub fn (mut inst BIPUSH) fetch_operands(mut reader BytecodeReader) {
+	inst.val = reader.read_i8()
+}
+
+pub fn (mut inst BIPUSH) execute(mut frame Frame) {
+	mut stack := frame.opreand_stack()
+	stack.push<int>(inst.val)
+}
+
+pub struct SIPUSH {
+mut:
+	val i16
+}
+
+pub fn (mut inst SIPUSH) fetch_operands(mut reader BytecodeReader) {
+	inst.val = reader.read_i16()
+}
+
+pub fn (mut inst SIPUSH) execute(mut frame Frame) {
+	mut stack := frame.opreand_stack()
+	stack.push<int>(inst.val)
+}
