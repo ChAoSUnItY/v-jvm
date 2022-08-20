9029e364c064415ccd55e09b9f4d64de9126850f
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-08-20 16:59:42 +0800

    add fstore instructions

---
+++ vjvm/instruction/store/fstore.v
@@ -0,0 +1,44 @@
+module store
+
+import vjvm.rtda { Frame, Object }
+import vjvm.instruction.base { Index8Instruction, NoOperandInstruction }
+
+pub struct FSTORE {
+	Index8Instruction
+}
+
+pub fn (mut inst FSTORE) execute(mut frame Frame) ! {
+	store<f32>(mut frame, inst.index)!
+}
+
+pub struct FSTORE_0 {
+	NoOperandInstruction
+}
+
+pub fn (mut inst FSTORE_0) execute(mut frame Frame) ! {
+	store<f32>(mut frame, 0)!
+}
+
+pub struct FSTORE_1 {
+	NoOperandInstruction
+}
+
+pub fn (mut inst FSTORE_1) execute(mut frame Frame) ! {
+	store<f32>(mut frame, 1)!
+}
+
+pub struct FSTORE_2 {
+	NoOperandInstruction
+}
+
+pub fn (mut inst FSTORE_2) execute(mut frame Frame) ! {
+	store<f32>(mut frame, 2)!
+}
+
+pub struct FSTORE_3 {
+	NoOperandInstruction
+}
+
+pub fn (mut inst FSTORE_3) execute(mut frame Frame) ! {
+	store<f32>(mut frame, 3)!
+}
