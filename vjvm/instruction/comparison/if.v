245a8a7bfb637d1d3c276d8d5dd3269691e6864b
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-08-21 21:00:45 +0800

    add ifx instructions

---
+++ vjvm/instruction/comparison/if.v
@@ -0,0 +1,70 @@
+module comparison
+
+import vjvm.rtda { Frame }
+import vjvm.instruction.base { BranchInstruction, branch }
+
+pub struct IFEQ {
+	BranchInstruction
+}
+
+pub fn (mut inst IFEQ) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	if stack.pop<int>()! == 0 {
+		branch(mut frame, inst.offset)
+	}
+}
+
+pub struct IFNE {
+	BranchInstruction
+}
+
+pub fn (mut inst IFNE) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	if stack.pop<int>()! != 0 {
+		branch(mut frame, inst.offset)
+	}
+}
+
+pub struct IFLT {
+	BranchInstruction
+}
+
+pub fn (mut inst IFLT) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	if stack.pop<int>()! < 0 {
+		branch(mut frame, inst.offset)
+	}
+}
+
+pub struct IFLE {
+	BranchInstruction
+}
+
+pub fn (mut inst IFLE) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	if stack.pop<int>()! <= 0 {
+		branch(mut frame, inst.offset)
+	}
+}
+
+pub struct IFGT {
+	BranchInstruction
+}
+
+pub fn (mut inst IFGT) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	if stack.pop<int>()! > 0 {
+		branch(mut frame, inst.offset)
+	}
+}
+
+pub struct IFGE {
+	BranchInstruction
+}
+
+pub fn (mut inst IFGE) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	if stack.pop<int>()! >= 0 {
+		branch(mut frame, inst.offset)
+	}
+}
