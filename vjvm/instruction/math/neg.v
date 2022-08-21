1cd92f3b65da91b479232ae61362359eb4271a56
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-08-21 17:05:37 +0800

    add neg instructions

---
+++ vjvm/instruction/math/neg.v
@@ -0,0 +1,44 @@
+module math
+
+import vjvm.rtda { Frame }
+import vjvm.instruction.base { NoOperandInstruction }
+
+pub struct DNEG {
+	NoOperandInstruction
+}
+
+pub fn (mut inst DNEG) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	val := stack.pop<f64>()!
+	stack.push<f64>(-val)
+}
+
+pub struct FNEG {
+	NoOperandInstruction
+}
+
+pub fn (mut inst FNEG) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	val := stack.pop<f32>()!
+	stack.push<f32>(-val)
+}
+
+pub struct INEG {
+	NoOperandInstruction
+}
+
+pub fn (mut inst INEG) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	val := stack.pop<int>()!
+	stack.push<int>(-val)
+}
+
+pub struct LNEG {
+	NoOperandInstruction
+}
+
+pub fn (mut inst LNEG) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	val := stack.pop<i64>()!
+	stack.push<i64>(-val)
+}
