2378c91ef637da1a796762d20ef36945cd65ad43
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-08-21 20:16:32 +0800

    add fcmp

---
+++ vjvm/instruction/comparison/cmp.v
@@ -0,0 +1,33 @@
+module comparison
+
+import vjvm.rtda { Frame }
+
+fn cmp_g<T>(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	val1, val2 := stack.pop2<T>()!
+	val := if val1 > val2 {
+		1
+	} else if val1 == val2 {
+		0
+	} else if val1 < val2 {
+		-1
+	} else {
+		1
+	}
+	stack.push<int>(val)
+}
+
+fn cmp_l<T>(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	val1, val2 := stack.pop2<T>()!
+	val := if val1 > val2 {
+		1
+	} else if val1 == val2 {
+		0
+	} else if val1 < val2 {
+		-1
+	} else {
+		-1
+	}
+	stack.push<int>(val)
+}
---
+++ vjvm/instruction/comparison/fcmp.v
@@ -0,0 +1,20 @@
+module comparison
+
+import vjvm.rtda { Frame }
+import vjvm.instruction.base { NoOperandInstruction }
+
+pub struct FCMPG {
+	NoOperandInstruction
+}
+
+pub fn (mut inst FCMPG) execute(mut frame Frame) ! {
+	cmp_g<f32>(mut frame)!
+}
+
+pub struct FCMPL {
+	NoOperandInstruction
+}
+
+pub fn (mut inst FCMPG) execute(mut frame Frame) ! {
+	cmp_l<f32>(mut frame)!
+}
