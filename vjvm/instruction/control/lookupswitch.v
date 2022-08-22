8435ba0c0d1a28b6b851818cbd91daad90e6ba2a
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-08-22 16:40:20 +0800

    add lookup_switch instruction

---
+++ vjvm/instruction/control/lookupswitch.v
@@ -0,0 +1,30 @@
+module control
+
+import vjvm.rtda { Frame }
+import vjvm.instruction.base { BytecodeReader, branch }
+
+pub struct LOOKUP_SWITCH {
+mut:
+	default_offset int
+	n_pairs        int
+	match_offsets  []int = []int{}
+}
+
+pub fn (mut inst LOOKUP_SWITCH) fetch_operands(mut reader BytecodeReader) ! {
+	reader.skip_padding()
+	inst.default_offset = reader.read_int()
+	inst.n_pairs = reader.read_int()
+	inst.match_offsets = reader.read_int_arr(inst.n_pairs * 2)
+}
+
+pub fn (mut inst LOOKUP_SWITCH) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	key := stack.pop<int>()!
+	for i := 0; i < inst.n_pairs * 2; i += 2 {
+		if inst.match_offsets[i] == key {
+			branch(mut frame, inst.match_offsets[i + 1])
+			return
+		}
+	}
+	branch(mut frame, inst.default_offset)
+}
