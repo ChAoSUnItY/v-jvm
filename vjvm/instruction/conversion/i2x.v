7406048af6946706be70a1b7e0ffdf397b67a5c9
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-08-26 10:05:06 +0800

    add i2b & i2s instructions

--- vjvm/instruction/conversion/i2x.v
+++ vjvm/instruction/conversion/i2x.v
@@ -3,6 +3,26 @@ module conversion
 import vjvm.rtda { Frame }
 import vjvm.instruction.base { NoOperandInstruction }
 
+pub struct I2B {
+	NoOperandInstruction
+}
+
+pub fn (mut inst I2B) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	val := stack.pop<int>()!
+	stack.push<int>(i8(val))
+}
+
+pub struct I2S {
+	NoOperandInstruction
+}
+
+pub fn (mut inst I2S) execute(mut frame Frame) ! {
+	mut stack := frame.opreand_stack()
+	val := stack.pop<int>()!
+	stack.push<int>(i16(val))
+}
+
 pub struct I2F {
 	NoOperandInstruction
 }
