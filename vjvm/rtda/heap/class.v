0b9dadbaae2411cf4df6d92e0b2b781cd0868eb0
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-09-01 23:24:37 +0800

    add class struct

---
+++ vjvm/rtda/heap/class.v
@@ -0,0 +1,39 @@
+module heap
+
+import vjvm.classfile { ClassFile }
+
+pub struct Class {
+	Access
+	name string
+	super_class_name string
+	interface_names []string
+	constant_pool ConstantPool
+	fields []Field
+	methods []Method
+	loader ClassLoader
+	super_class &Class
+	interfaces []&Class
+	instance_slot_count u32
+	static_slot_count u32
+	static_slots Slots
+}
+
+pub fn (mut class Class) from_class_file(cf &ClassFile) {
+	
+}
+
+pub fn (class &Class) is_interface() bool {
+	return class.access_flags & acc_interface != 0
+}
+
+pub fn (class &Class) is_abstract() bool {
+	return class.access_flags & acc_abstract != 0
+}
+
+pub fn (class &Class) is_annotation() bool {
+	return class.access_flags & acc_annotation != 0
+}
+
+pub fn (class &Class) is_enum() bool {
+	return class.access_flags & acc_enum != 0
+}
