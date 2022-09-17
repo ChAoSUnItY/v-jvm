633273e8629908f21f3cd64dedcb6d951a6f1e24
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-09-18 00:16:33 +0800

    Revamp stack operation impl

--- vjvm/rtda/local_vars.v
+++ vjvm/rtda/local_vars.v
@@ -15,41 +15,56 @@ fn new_local_vars(max_locals u32) &LocalVars {
 	}
 }
 
-pub fn (mut local LocalVars) set<T>(val T, index u32) {
-	$if T is Object {
-		local.slots[index].ref = &val
-	} $else $if T is int {
-		local.slots[index].num = val
-	} $else $if T is f32 {
-		local.slots[index].num = int(f32_bits(val))
-	} $else $if T is i64 {
-		local.slots[index].num = int(val)
-		local.slots[index + 1].num = int(val >> 32)
-	} $else $if T is f32 {
-		bits := i64(f32_bits(val))
-		local.set(bits, index)
-	} $else $if T is f64 {
-		bits := i64(f64_bits(val))
-		local.set(bits, index)
-	}
+[inline]
+pub fn (mut local LocalVars) set_int(val int, index u32) {
+	local.slots[index].num = val
 }
 
-pub fn (local LocalVars) get<T>(index u32) !T {
-	$if T is Object {
-		return *local.slots[index].ref
-	} $else $if T is int {
-		return local.slots[index].num
-	} $else $if T is f32 {
-		return f32_from_bits(u32(local.slots[index].num))
-	} $else $if T is i64 {
-		low := local.slots[index].num
-		high := local.slots[index - 1].num
-		return i64(high) << 32 | i64(low)
-	} $else $if T is f32 {
-		return f32_from_bits(u32(local.slots[index].num))
-	} $else $if T is f64 {
-		return f64_from_bits(u64(local.get<i64>(index)!))
-	} $else {
-		return error('$T.name is not a valid Slot item')
-	}
+[inline]
+pub fn (mut local LocalVars) set_f32(val f32, index u32) {
+	local.slots[index].num = int(f32_bits(val))
+}
+
+[inline]
+pub fn (mut local LocalVars) set_i64(val i64, index u32) {
+	local.slots[index].num = int(val)
+	local.slots[index + 1].num = int(val >> 32)
+}
+
+[inline]
+pub fn (mut local LocalVars) set_f64(val f64, index u32) {
+	bits := i64(f32_bits(val))
+	local.set_i64(bits, index)
+}
+
+[inline]
+pub fn (mut local LocalVars) set_ref(val &Object, index u32) {
+	local.slots[index].ref = val
+}
+
+[inline]
+pub fn (mut local LocalVars) get_int(index u32) int {
+	return local.slots[index].num
+}
+
+[inline]
+pub fn (mut local LocalVars) get_f32(index u32) f32 {
+	return f32_from_bits(u32(local.slots[index].num))
+}
+
+[inline]
+pub fn (mut local LocalVars) get_i64(index u32) i64 {
+	low := local.slots[index].num
+	high := local.slots[index - 1].num
+	return i64(high) << 32 | i64(low)
+}
+
+[inline]
+pub fn (mut local LocalVars) get_f64(index u32) f64 {
+	return f64_from_bits(u64(local.get_i64(index)))
+}
+
+[inline]
+pub fn (mut local LocalVars) get_ref(index u32) &Object {
+	return local.slots[index].ref
 }
--- vjvm/rtda/slot.v
+++ vjvm/rtda/slot.v
@@ -7,29 +7,3 @@ mut:
 	num int
 	ref &Object
 }
-
-[inline]
-fn (slot &Slot) conv<T>() !T {
-	$if T is Object {
-		return *slot.ref
-	} $else $if T is int {
-		return slot.num
-	} $else $if T is f32 {
-		return f32_from_bits(u32(slot.conv<int>()!))
-	} $else $if T is Slot {
-		return slot
-	} $else {
-		return error('$T.name is not a valid slot item')
-	}
-}
-
-[inline]
-fn (slot1 &Slot) conv2<T>(slot2 &Slot) !T {
-	$if T is i64 {
-		return i64(slot1.num) << 32 | i64(slot2.num)
-	} $else $if T is f64 {
-		return f64_from_bits(u64(slot1.conv2<i64>(slot2)!))
-	} $else {
-		return error('$T.name is not a valid slot item')
-	}
-}
